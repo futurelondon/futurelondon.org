d6f470b3fe1998cf9233e0a7f8d92699b9fb6ae1
Jamie Rumbelow <jamie@jamierumbelow.net>
2023-10-01 13:37:45 +0100

    mobile

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,9 @@ module.exports = {
   content: ["./src/**/*.html"],
   theme: {
     extend: {
+      colors: {
+        brand: "rgb(53,32,101)",
+      },
       fontFamily: {
         header: ["Staatliches"],
         title: ["Pixelify Sans"],
